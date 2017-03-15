c0281e71fb496dfacd5e8652a996f7ffa64185a2
meerestier <lars.schulz@systemic.de>
2017-03-15 18:15:19 +0100

    Basic Plotting

--- korngroessen.R
+++ korngroessen.R
@@ -4,8 +4,8 @@
 
 # Libraries
 # install.packages("aqp") #momentan nicht benötigt aber interessante Funktionen
-# library("aqp")
-library("gdata")
+# library("aqp") # Für Horizont-Plots
+library("gdata") # Für Datenimport und -manipulation etc.
 
 # Arbeitsverzeichnis auswählen
 setwd("~/Dropbox/Jobs Active/Studium/TU-1611 Kiecker Naturschutzgebiet/1-Entwicklung/9-Laborarbeiten/Korngroessen")
@@ -22,9 +22,16 @@ colnames(korngroessen[-1,45:320])
 
 # Simplify data
 korngroessen_data <- cbind(korngroessen[,'Sample.Name'], korngroessen[,45:320])
-colnames(korngroessen_data)[1] <- "Sample.Name"
+colnames(korngroessen_data)[1] <- "Sample"
 str(korngroessen_data)
 attach(korngroessen_data)
+
+# Optional
+      # Anreichern mit zusätlichen Informationen (auskommentieren, falls nicht erforderlich)
+      # horizont_infos <- read.xls("~/Dropbox/Jobs Active/Studium/TU-1611 Kiecker Naturschutzgebiet/1-Entwicklung/9-Laborarbeiten/Probenliste v3.xls", 6) # Probennummern holen
+      # colnames(horizont_infos)[3] <- "Sample"
+      # korngroessen_data <- merge(korngroessen_data, horizont_infos, by = "Sample")
+      # str(korngroessen_data)
 
 # Umbenennen
 zeile_1 <- korngroessen_data[1,]
@@ -32,7 +39,7 @@ names(zeile_1) <- NULL
 zeile_1
 new_headers <- paste (head( c("CHECK",zeile_1[,-1]), -1 ), "µm")
 
-names(korngroessen_data) <- c("Sample.Name", new_headers)
+names(korngroessen_data) <- c("Sample", new_headers)
 str(korngroessen_data)
 
 # modulo, jede dritte Spalte auswählen (Spalte q in Rohdaten)
@@ -45,10 +52,11 @@ korngroessen_clean <- cbind(korngroessen_data[,1], korngroessen_data[, c%%3==0])
 variables_in_cols <- names(korngroessen_clean[,-1])
 
 # Mittel pro Faktorstufe
-korngroessen_stats <- aggregate(korngroessen_clean[, variables_in_cols], list(Sample.Name), mean)
+korngroessen_stats <- aggregate(korngroessen_clean[, variables_in_cols], list(Sample), mean)
 
 # Create overview of classes
 korngroessen_overview <- as.data.frame(korngroessen_stats[,1])
+colnames(korngroessen_overview)[1] <- "Sample"
 
 korngroessen_overview$ton <- rowSums(korngroessen_stats[, 2:40]) # ton insgesamt
 korngroessen_overview$schluff <- rowSums(korngroessen_stats[, 41:65]) # schluff insgesamt
@@ -68,9 +76,29 @@ korngroessen_overview$fS <- rowSums(korngroessen_stats[, 66:74]) # feinsand
 korngroessen_overview$mS <- rowSums(korngroessen_stats[, 75:82]) # mittelsand
 korngroessen_overview$gS <- rowSums(korngroessen_stats[, 83:93]) # grobsand
 
-
+attach(korngroessen_overview)
 # Klassifikation nach KA-5
 # TODO
 
-write.csv(korngroessen_overview, file="korngroessen_overview.csv")
+write.csv(korngroessen_overview, file="korngroessen_overview.csv", fileEncoding = "UTF-8")
+
+# Darstellung ----
+
+matrix_names <- t (korngroessen_overview[,1])
+korngroessen_matrix <- t (korngroessen_overview[,c(2,8:13)]) # matrix und transposed: Ton als eine Faktorstufe, Schluff und Sand aufgesplittet
+str(korngroessen_matrix)
+barplot(korngroessen_matrix )
+
+
+x <- barplot(korngroessen_matrix, col=NULL, 
+             legend=TRUE, border=NA, xlim=NULL, args.legend=
+               list(bty="n", border=NA), 
+             ylab="Cumulative percentage", xlab="Sample", las=2, names=matrix_names)
+text(x, ton/2, labels=round(korngroessen_overview$ton), col="black", srt = 90)
+text(x, ton+fU/2, labels=round(korngroessen_overview$fU), col="black", srt = 90)
+text(x, ton+fU+mU/2, labels=round(korngroessen_overview$mU), col="black", srt = 90)
+text(x, ton+fU+mU+gU/2, labels=round(korngroessen_overview$gU), col="black", srt = 90)
+text(x, ton+fU+mU+gU+fS/2, labels=round(korngroessen_overview$gU), col="black", srt = 90)
+text(x, ton+fU+mU+gU+fS+mS/2, labels=round(korngroessen_overview$gU), col="black", srt = 90)
+text(x, ton+fU+mU+gU+fS+mS+gS/2, labels=round(korngroessen_overview$gU), col="black", srt = 90)
 
